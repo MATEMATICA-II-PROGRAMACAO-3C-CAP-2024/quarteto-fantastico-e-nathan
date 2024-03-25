f050df73b75906c3c25242ddc55334a5e0de47eb
Quinteto-fantastico30102006 <128146736+Quinteto-fantastico30102006@users.noreply.github.com>
2024-03-25 08:49:05 -0300

    aula 6

--- main.js
+++ main.js
@@ -3,7 +3,7 @@ const textos = document.querySelectorAll(".aba-conteudo");
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date ("2024-10-05T00:00:00");
 contadores[0]
-contadores[0].textContent= tempoObjetivo1;
+contadores[0].textContent= calculdoraTempo (tempoObjetivo1);
 
 for (let i = 0; i < botoes.length; i++) {
     botoes[i].onclick = function () {
@@ -15,4 +15,10 @@ for (let i = 0; i < botoes.length; i++) {
         botoes[i].classList.add("ativo");
         textos[i].classList.add("ativo");
     }
+}  function calculadoraTempo (tempoObjetivo){
+     let tempoAtual = new Date();
+     let tempoFinal = tempoObjetivo - tempoAtual;
+     let segundos = Math.floor (tempoFinal / 1000); 
+     let minutos = Marh.floor (segundos / 60);
+     let horas = Math.floor(minutos / 60); 
 }
